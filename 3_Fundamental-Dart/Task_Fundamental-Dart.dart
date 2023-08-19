76e59170fcfc920cb582b7ddd7bdf0f5f1ddb4b1
firiyah.apps <firiyah.apps@gmail.com>
2023-08-19 13:48:24 +0700

    Submit Task

---
+++ 3_Fundamental-Dart/Task_Fundamental-Dart.dart
@@ -0,0 +1,19 @@
+import 'dart:math';
+
+void main() {
+  LuasLingkaran();
+  Interpolasi();
+}
+
+LuasLingkaran() {
+  var r = 7;
+  var luas = pi * r * r;
+  print(luas);
+}
+
+Interpolasi() {
+  String s1 = "Saya sedang";
+  String s2 = "mengikuti study independen";
+  String s3 = "di Alterra Academy.";
+  print("$s1 $s2 $s3");
+}
