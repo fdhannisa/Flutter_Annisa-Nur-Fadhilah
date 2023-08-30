e78904087416279de21c5c72338cb85d76bf6ad8
annisa <92286817+fdhannisa@users.noreply.github.com>
2023-08-30 19:19:09 +0700

    Contoh implementasi oop

--- 6_Dart-Object-Oriented-Programing/dart_oop.dart
+++ 6_Dart-Object-Oriented-Programing/dart_oop.dart
@@ -1,7 +1,27 @@
+//import 'constructor.dart';
+//bisa digunakan jika constructor dalam bentuk file sendiri. Untuk memanggil constructor
+
 class Hewan {
   int mata = 2;
   int jari = 5;
   String nama = "Guuk";
+
+//materi deep dive
+  int? kaki;
+
+//Konstruktor
+  Hewan() {
+    mata = 0;
+    kaki = 0;
+  }
+
+  reproduksi() {
+    print("tidak tahu");
+  }
+
+  bernafas() {
+    print("Menggunakan : ");
+  }
 }
 
 class Kucing {
@@ -9,6 +29,26 @@ class Kucing {
     print("Meong..");
   }
 }
+
+//inheritence
+class Kambing extends Hewan {
+  Kambing() {
+    mata = 4;
+    kaki = 8;
+  }
+
+  @override
+  bernafas() {
+    print("Bernafas menggunakan : Paru-Paru");
+  }
+
+  @override
+  reproduksi() {
+    print("Melahirkan");
+  }
+}
+
+//interface
 
 void main() {
   var h1 = Hewan();
@@ -18,4 +58,19 @@ void main() {
 
   var h2 = Kucing();
   h2.bersuara();
+
+  //materi deep dive
+  //object baru berupa hewan dengan menggunakan class Hewan
+  var hewan = Hewan();
+  //ngambil niali dari konstruktor
+  print(hewan.kaki);
+  print(hewan.mata);
+
+  var k1 = Kambing();
+  print(k1.mata);
+  print(k1.kaki);
+
+  //manggil method override pada class kambing
+  k1.bernafas();
+  k1.reproduksi();
 }
